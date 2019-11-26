6da1e442267aba934b7db63274e6b636b51d5cb8
Jakub Joniec <kuba222kuba@gmail.com>
2019-11-27 00:11:00 +0100

    Add dots solution but it's to fix

---
+++ scripts/dots.js
@@ -0,0 +1,38 @@
+/**
+ * dots.js v1.0.0
+ * http://www.codrops.com
+ *
+ * Licensed under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ * 
+ * Copyright 2014, Codrops
+ * http://www.codrops.com
+ */
+;( function( window ) {
+
+	'use strict';
+
+	function extend( a, b ) {
+		for( var key in b ) { 
+			if( b.hasOwnProperty( key ) ) {
+				a[key] = b[key];
+			}
+		}
+		return a;
+	}
+
+	function DotNav( el, options ) {
+		this.nav = el;
+		this.options = extend( {}, this.options );
+  		extend( this.options, options );
+  		this._init();
+	}
+
+	DotNav.prototype.options = {};
+
+	
+
+	// add to global namespace
+	window.DotNav = DotNav;
+
+})( window );
